90194a7d6f2264c61f9e6b0d9e5ddfb8c7a2709c
gt.park <smileboy0014@gmail.com>
2024-06-24 11:54:46 +0900

    Refector : "PointService 리팩토링" - 객체지향적으로 변경

--- src/main/java/io/hhplus/tdd/point/service/PointService.java
+++ src/main/java/io/hhplus/tdd/point/service/PointService.java
@@ -3,6 +3,8 @@ package io.hhplus.tdd.point.service;
 import io.hhplus.tdd.point.common.LockHelper;
 import io.hhplus.tdd.point.domain.PointHistory;
 import io.hhplus.tdd.point.domain.UserPoint;
+import io.hhplus.tdd.point.enums.TransactionType;
+import io.hhplus.tdd.point.exception.ErrorCode;
 import io.hhplus.tdd.point.exception.PointException;
 import io.hhplus.tdd.point.repository.PointHistoryRepository;
 import io.hhplus.tdd.point.repository.UserPointRepository;
@@ -10,6 +12,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 import static io.hhplus.tdd.point.enums.TransactionType.CHARGE;
 import static io.hhplus.tdd.point.enums.TransactionType.USE;
@@ -21,80 +26,62 @@ import static io.hhplus.tdd.point.exception.ErrorCode.NOT_ENOUGH_POINT;
 public class PointService {
 
     private final UserPointRepository userPointRepository;
-
     private final PointHistoryRepository pointHistoryRepository;
-
     private final LockHelper lockHelper;
-
 
     //포인트 조회
     public UserPoint getPoint(long id) {
-
         return userPointRepository.selectById(id);
     }
 
     //포인트 충전
     public UserPoint charge(long id, long amount) {
-
-        // 포인트의 유효성 체크
-        if (!validPoint(amount)) {
-            throw new PointException(INVALID_CHARGE_POINT, "0보다 작은 포인트는 충전되지 않습니다.");
-        }
-
-        // 동시성 이슈를 개선하기 위해 lockHelper 를 사용
-        return lockHelper.executeWithLock(id, () -> {
-
-            UserPoint curUser = userPointRepository.selectById(id);
-
-            // 포인트 충전
-            UserPoint userPoint = userPointRepository.insertOrUpdate(id, curUser.point() + amount);
-
-            // 포인트 충전 내역 추가
-            pointHistoryRepository.insert(id, amount, CHARGE, System.currentTimeMillis());
-
-            return userPoint;
-        });
+        validateAmount(amount, INVALID_CHARGE_POINT, "0보다 작은 포인트는 충전되지 않습니다.");
+        return executeWithLockAndUpdate(id, amount, CHARGE, this::addPoints);
     }
 
     //포인트 사용
     public UserPoint use(long id, long amount) {
-
-        // 포인트의 유효성 체크
-        if (!validPoint(amount)) {
-            throw new PointException(INVALID_CHARGE_POINT, "0보다 작은 포인트는 사용할 수 없습니다.");
-        }
-        // 동시성 이슈를 개선하기 위해 lockHelper 를 사용
-        return lockHelper.executeWithLock(id, () -> {
-            UserPoint curUser = userPointRepository.selectById(id);
-
-            // 포인트가 부족하지 않은지 체크
-            if (!isPossibleUse(amount, curUser.point())) {
-                throw new PointException(NOT_ENOUGH_POINT, "포인트가 부족합니다.");
-            }
-
-            // 포인트 차감
-            UserPoint userPoint = userPointRepository.insertOrUpdate(id, curUser.point() - amount);
-
-            // 포인트 차감 내역 추가
-            pointHistoryRepository.insert(id, amount, USE, System.currentTimeMillis());
-
-            return userPoint;
-        });
+        validateAmount(amount, INVALID_CHARGE_POINT, "0보다 작은 포인트는 사용할 수 없습니다.");
+        return executeWithLockAndUpdate(id, amount, USE, this::subtractPoints);
     }
 
     //포인트 내역 조회
     public List<PointHistory> getHistory(long id) {
-
         return pointHistoryRepository.selectAllByUserId(id);
     }
 
-    // 충전하려는 포인트가 0 미만인지 체크
-    private boolean validPoint(long amount) {
+    private void validateAmount(long amount, ErrorCode errorCode, String errorMessage) {
+        if (!isValidPoint(amount)) {
+            throw new PointException(errorCode, errorMessage);
+        }
+    }
+
+    private boolean isValidPoint(long amount) {
         return amount >= 0;
     }
 
-    // 사용하려는 포인트가 남아있는 포인트보다 적은지 체크
-    private boolean isPossibleUse(long amount, long userPoint) {
-        return amount <= userPoint;
+    private UserPoint executeWithLockAndUpdate(long id, long amount, TransactionType type, BiFunction<UserPoint, Long, UserPoint> operation) {
+        return lockHelper.executeWithLock(id, () -> {
+            UserPoint curUser = userPointRepository.selectById(id);
+            UserPoint updatedUser = operation.apply(curUser, amount);
+            pointHistoryRepository.insert(id, amount, type, System.currentTimeMillis());
+            return updatedUser;
+        });
+    }
+
+    private UserPoint addPoints(UserPoint user, long amount) {
+        return userPointRepository.insertOrUpdate(user.id(), user.point() + amount);
+    }
+
+    private UserPoint subtractPoints(UserPoint user, long amount) {
+        if (!hasEnoughPoints(user, amount)) {
+            throw new PointException(NOT_ENOUGH_POINT, "포인트가 부족합니다.");
+        }
+        return userPointRepository.insertOrUpdate(user.id(), user.point() - amount);
+    }
+
+    private boolean hasEnoughPoints(UserPoint user, long amount) {
+        return amount <= user.point();
     }
 }
